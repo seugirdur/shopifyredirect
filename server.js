eef285f477a3e3b809e7441f9ca801874e504c52
seugirdur <98267778+seugirdur@users.noreply.github.com>
2023-05-12 01:12:20 -0300

    teste

--- server.js
+++ server.js
@@ -1,46 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-var cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+var cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.use(cors());
 
+app.get("/", async function (req, res) {
+  res.send("Hello World");
+});
 
-app.use(cors())
+app.post("/", bodyParser.text({ type: "*/*" }), async function (req, res) {
+  try {
+    console.log(req.body);
+    const lmao = req.body;
 
+    res.send(lmao);
+  } catch (err) {
+    console.log("erro: => " + err);
+  }
+});
 
+app.post("/login", urlencodedParser, function (req, res) {
+  res.send("welcome, " + req.body.username);
+});
 
-app.get("/", async function(req, res) {
-res.send("Hello World");
-})
-
-
-
-app.post("/",  bodyParser.text({type: '*/*'}), async function(req, res) {
-    try {
-  
-      console.log(req.body);
-
-      // const lmao = JSON.parse(req.body)
-      const lmao = req.body
-      // JSON.stringify(req.body)
-
-      // res.status(200).json({
-      //   teste: "teste",
-      // });
-      res.send(lmao)
-    } catch (err) {
-      console.log("erro: => " + err);
-    }
-  });
-
-  app.post('/login', urlencodedParser, function (req, res) {
-    res.send('welcome, ' + req.body.username)
-  })
-
-app.listen(3333);
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server running on port ${port}`));
